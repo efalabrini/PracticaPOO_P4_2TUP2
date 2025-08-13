b52ec67459b19f9127b2fee064f38c580a20c2cf
AlvaroReynoso <areynoso@frro.utn.edu.ar>
2025-08-13 14:12:59 -0300

    Add BankAccount class with properties and methods for deposits and withdrawals

---
+++ src/Core/Entities/BankAccounts.cs
@@ -0,0 +1,17 @@
+// Nombrar correctamente por directorios
+namespace PracticaPOO_P4_2TUP2.Core.Entities;
+
+public class BankAccount
+{
+    public string Number { get; }
+    public string Owner { get; set; }
+    public decimal Balance { get; }
+
+    public void MakeDeposit(decimal amount, DateTime date, string note)
+    {
+    }
+
+    public void MakeWithdrawal(decimal amount, DateTime date, string note)
+    {
+    }
+}
