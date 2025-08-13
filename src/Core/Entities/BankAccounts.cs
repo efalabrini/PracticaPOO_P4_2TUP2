aa2c306ec0dcc687c8e02a84a55fa86605940a6f
AlvaroReynoso <areynoso@frro.utn.edu.ar>
2025-08-13 14:50:51 -0300

    Refactor BankAccount class to use properties for Number and Balance; add AccountController for account creation

--- src/Core/Entities/BankAccounts.cs
+++ src/Core/Entities/BankAccounts.cs
@@ -3,15 +3,14 @@ namespace PracticaPOO_P4_2TUP2.Core.Entities;
 
 public class BankAccount
 {
-    public string Number { get; }
+    public string Number { get; set; }
     public string Owner { get; set; }
-    public decimal Balance { get; }
+    public decimal Balance { get; private set; }
 
-    public void MakeDeposit(decimal amount, DateTime date, string note)
+    public BankAccount(string number, decimal initialBalance = 0)
     {
+        Number = number;
+        Balance = initialBalance;
     }
 
-    public void MakeWithdrawal(decimal amount, DateTime date, string note)
-    {
-    }
 }
