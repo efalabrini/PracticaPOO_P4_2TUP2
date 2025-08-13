f84936c1b986e531d5b8a1dd2d42393974ead572
Marino-404 <marinorova25@gmail.com>
2025-08-13 15:06:26 -0300

    Create class BankAccount

--- src/Core/Entities/BankAccount.cs
+++ src/Core/Entities/BankAccount.cs
@@ -3,16 +3,23 @@ namespace Core.Entities;
 public class BankAccount
 {
     public string Number { get; }
-
     public string Owner { get; set; }
+    public decimal Balance { get; private set; }
 
-    public string Balance { get; }
+    public BankAccount(string number, string owner, decimal initialBalance)
+    {
+        Number = number;
+        Owner = owner;
+        Balance = initialBalance;
+    }
 
     public void MakeDeposit(decimal amount, DateTime date, string note)
     {
+        Balance += amount;
     }
 
     public void MakeWithDrawal(decimal amount, DateTime date, string note)
     {
+        Balance -= amount;
     }
-} 
+}
