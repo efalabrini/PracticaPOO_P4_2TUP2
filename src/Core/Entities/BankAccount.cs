9451738c807bad24672da45ec37583462f5164e3
FerPrincipal <fr30031@gmail.com>
2025-08-13 15:00:25 -0300

    BankAccount modified and Create Transaction

--- src/Core/Entities/BankAccount.cs
+++ src/Core/Entities/BankAccount.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Entities;
 
 namespace Core.Entities
 {
@@ -10,19 +11,48 @@ namespace Core.Entities
     {
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
-
+        private static int s_accountNumberSeed = 1234567890;
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in _allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+        private List<Transaction> _allTransactions = new List<Transaction>();
         public BankAccount(string name, decimal initialBalance)
         {
+            Number = s_accountNumberSeed.ToString();
+            s_accountNumberSeed++;
+
             Owner = name;
-            Balance = initialBalance;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
         }
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(amount), "Deposit amount must be positive.");
+
+            var deposit = new Transaction(amount, date, note);
+            _allTransactions.Add(deposit);
         }
 
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(amount), "Withdrawal amount must be positive.");
+
+            if (Balance - amount < 0)
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal.");
+
+            var withdrawal = new Transaction(-amount, date, note);
+            _allTransactions.Add(withdrawal);
         }
     }
+
 }
