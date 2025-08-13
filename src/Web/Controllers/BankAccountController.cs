1e3b661da30e6101cb1373ee1f8cbe48c2018d49
FerPrincipal <fr30031@gmail.com>
2025-08-13 14:30:14 -0300

    Clase1308

---
+++ src/Web/Controllers/BankAccountController.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BankAccountController : ControllerBase
+    {
+
+        [HttpPost("BankAccountCreate")]
+        public ActionResult<BankAccount> CreateBankAccount([FromQuery] string name, [FromQuery] decimal initialBalance)
+        {
+            var account = new BankAccount(name, initialBalance);
+            return Ok(account);
+        }
+    }
+}
