b9f1ba02156425b68a48fb553e3afd2ea765903b
MDG-22 <116525431+MDG-22@users.noreply.github.com>
2025-08-13 14:02:30 -0300

    Espero que este bien hecho

--- src/Core/Entities/BankAccount.cs
+++ src/Core/Entities/BankAccount.cs
@@ -8,5 +8,23 @@ namespace Core.Entities
 {
     internal class BankAccount
     {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        { 
+        }
+
     }
 }
