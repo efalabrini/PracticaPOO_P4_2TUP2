cbec4051185c3e3b7c7c006ff3c8979306d6f62f
ggabi40 <gabibc2000@hotmail.com>
2025-08-13 14:23:20 -0300

    New Controller: Create New BankAccount

--- src/Core/Entities/BankAccount.cs
+++ src/Core/Entities/BankAccount.cs
@@ -2,7 +2,7 @@ namespace Core.Entities
 {
     public class BankAccount
     {
-        public string Number { get; }
+        public string Number { get; } = string.Empty;
         public string Owner { get; set; }
         public decimal Balance { get; }
 
@@ -13,7 +13,14 @@ namespace Core.Entities
 
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
-            
+
+        }
+
+        // Constructor:
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
         }
     }
 }
