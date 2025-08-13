52e104413f7e66d8bddaad68b99feacbeb8d2ea0
Luciano Tessa <tessaluciano3@gmail.com>
2025-08-13 14:17:08 -0300

    add bankAccount

--- Core/Entities/BankAccount.cs
+++ Core/Entities/BankAccount.cs
@@ -1,21 +1,18 @@
 using System;
 
-namespace Entities
+namespace Classes;
+
+public class BankAccount
 {
-    public class BankAccount
+    public string Number { get; }
+    public string Owner { get; set; }
+    public decimal Balance { get; }
+
+    public void MakeDeposit(decimal amount, DateTime date, string note)
     {
-        public string Number { get; }
-        public string Owner { get; set; }
-        public decimal Balance { get; }
+    }
 
-        private static int accountNumberSeed = 1234567890;
-
-        public BankAccount(string name, decimal initialBalance)
-        {
-            this.Owner = name;
-            this.Balance = initialBalance;
-            this.Number = accountNumberSeed.ToString();
-            accountNumberSeed++;
-        }
+    public void MakeWithdrawal(decimal amount, DateTime date, string note)
+    {
     }
 }
