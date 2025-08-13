56ee6bf9c8cea6955d201e82038148abfbff6680
Justina-R <justina.rey2602@gmail.com>
2025-08-13 15:23:31 -0300

    Create BankAccount Controller and upadate BankAccount class

---
+++ src/Core/Entities/BankAccount.cs
@@ -0,0 +1,25 @@
+namespace Core.Entities;
+
+public class BankAccount
+{
+    private static int s_accountNumberSeed = 1234567890;
+    public string Number { get; }
+    public string Owner { get; set; }
+    public decimal Balance { get; }
+
+    public BankAccount(string name, decimal initialBalance)
+    {
+        Owner = name;
+        Balance = initialBalance;
+
+        Number = s_accountNumberSeed.ToString();
+        s_accountNumberSeed++;
+    }
+    public void MakeDeposit(decimal amount, DateTime date, string note)
+    {
+    }
+
+    public void MakeWithdrawal(decimal amount, DateTime date, string note)
+    {
+    }
+}
