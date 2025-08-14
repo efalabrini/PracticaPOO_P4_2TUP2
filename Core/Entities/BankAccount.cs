5379cdcd586a235a8762b773c199610ed00bf20c
Luciano Tessa <tessaluciano3@gmail.com>
2025-08-14 17:16:58 -0300

    Agregado endpoint de bankAccount

--- Core/Entities/BankAccount.cs
+++ Core/Entities/BankAccount.cs
@@ -9,10 +9,25 @@ public class BankAccount
     public decimal Balance { get; }
 
     public void MakeDeposit(decimal amount, DateTime date, string note)
+{
+    if (amount <= 0)
     {
+        throw new ArgumentOutOfRangeException(nameof(amount), "la cantidad del depósito debe ser positiva");
     }
+    Balance += amount;
+}
 
     public void MakeWithdrawal(decimal amount, DateTime date, string note)
+{
+    if (amount <= 0)
     {
+        throw new ArgumentOutOfRangeException(nameof(amount), "la cantidad del retiro debe ser positiva");
     }
+    if (amount > Balance)
+    {
+        throw new InvalidOperationException("Nno hay suficiente saldo para realizar el retiro");
+    }
+    Balance -= amount;
+}
+
 }
