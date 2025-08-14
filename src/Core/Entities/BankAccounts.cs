0df951290023748b78d24ad6ebc68e96702d1b14
AlvaroReynoso <areynoso@frro.utn.edu.ar>
2025-08-14 17:41:02 -0300

    Add BankAccount and Transaction classes with initial properties and methods

--- src/Core/Entities/BankAccounts.cs
+++ src/Core/Entities/BankAccounts.cs
@@ -1,4 +1,5 @@
-// Nombrar correctamente por directorio
+using Classes;
+
 namespace PracticaPOO_P4_2TUP2.Core.Entities;
 
 public class BankAccount
@@ -7,10 +8,18 @@ public class BankAccount
     public string Owner { get; set; }
     public decimal Balance { get; private set; }
 
-    public BankAccount(string number, decimal initialBalance = 0)
+    private static int s_accountNumberSeed = 1234567890;
+
+
+    private List<Transaction> _allTransactions = [];
+
+
+    public BankAccount(string owner, decimal initialBalance = 0)
     {
-        Number = number;
+        Owner = owner;
         Balance = initialBalance;
+        Number = s_accountNumberSeed.ToString();
+        s_accountNumberSeed++;
     }
 
 }
