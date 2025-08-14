79b975796e8b3463318dc1be7f56d2f164f17340
santiange <santiangelica410@gmail.com>
2025-08-14 17:13:03 -0300

    fix problems

---
+++ src/Core/entities/BankAccount.cs
@@ -0,0 +1,66 @@
+namespace Core.Entities;
+
+public class BankAccount
+{
+    private static int s_accountNumberSeed = 1234567890;
+    public string Number { get; }
+    public string Owner { get; set; }
+    public decimal Balance
+    {
+        get
+        {
+            decimal balance = 0;
+            foreach (var item in _allTransactions)
+            {
+                balance += item.Amount;
+            }
+
+            return balance;
+        }
+    }
+
+
+    public BankAccount(string name, decimal initialBalance)
+    {
+        Owner = name;
+
+        Number = s_accountNumberSeed.ToString();
+        s_accountNumberSeed++;
+
+        MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+    }
+
+    private List<Transaction> _allTransactions = new List<Transaction>();
+
+    public Transaction MakeDeposit(decimal amount, DateTime date, string note)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+        }
+        var deposit = new Transaction(amount, date, note);
+        _allTransactions.Add(deposit);
+        return deposit;
+    }
+
+    public Transaction MakeWithdrawal(decimal amount, DateTime date, string note)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+        }
+        if (Balance - amount < 0)
+        {
+            throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+        }
+        var withdrawal = new Transaction(-amount, date, note);
+        _allTransactions.Add(withdrawal);
+        return withdrawal;
+    }
+
+  
+
+}
+
+
+
