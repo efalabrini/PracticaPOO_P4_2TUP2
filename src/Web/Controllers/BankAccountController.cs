8badb3a4fa53a5552d02a391c732fe2aa163ed1b
santiange <santiangelica410@gmail.com>
2025-08-14 17:08:52 -0300

    Create Transactions Class, add MakeDeposit and MakeWithdrawal

--- src/Web/Controllers/BankAccountController.cs
+++ src/Web/Controllers/BankAccountController.cs
@@ -8,8 +8,10 @@ namespace Web.Controllers;
 
 public class BankAccountController : ControllerBase
 {
+    private static readonly List<BankAccount> accounts = new List<BankAccount>();
+
     [HttpPost]
-    public IActionResult POSTnewAccount([FromQuery] string name , [FromQuery] decimal balance)
+    public IActionResult POSTnewAccount([FromQuery] string name, [FromQuery] decimal balance)
     {
         try
         {
@@ -18,11 +20,92 @@ public class BankAccountController : ControllerBase
             {
                 return BadRequest("Error al crear la cuenta");
             }
+            accounts.Add(newAccount);
             return Ok($"Nueva cuenta creada:\nNumer: {newAccount.Number}\nDueño: {newAccount.Owner}\nBalance inicial: {newAccount.Balance}");
         }
-        catch (Exception )
+        catch (Exception)
         {
             return StatusCode(500, "error interno del server");
         }
     }
+
+    [HttpGet]
+    public IActionResult GETallAccounts()
+    {
+        try
+        {
+            return Ok(accounts);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
+    }
+
+    [HttpPost("deposit/{aNumber}")]
+    public IActionResult POSTdeposit([FromBody] Transaction depositData, [FromRoute] string aNumber)
+    {
+        try
+        {
+            var account = accounts.FirstOrDefault(a => a.Number == aNumber);
+            if (account == null)
+            {
+                return NotFound($"Account with id: {aNumber} not found");
+            }
+            var deposit = account.MakeDeposit(depositData.Amount, depositData.Date, depositData.Notes);
+            if (deposit == null)
+            {
+                throw new Exception("Internal server error");
+            }
+            return Ok(new { deposit = deposit, balance = account.Balance });
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
+    }
+
+    [HttpPost("withdrawal/{aNumber}")]
+    public IActionResult POSTwithdrawal([FromBody] Transaction withdrawalData, [FromRoute] string aNumber)
+    {
+        try
+        {
+            var account = accounts.FirstOrDefault(a => a.Number == aNumber);
+            if (account == null)
+            {
+                return NotFound($"Account with id: {aNumber} not found");
+            }
+            var withdrawal = account.MakeWithdrawal(withdrawalData.Amount, withdrawalData.Date, withdrawalData.Notes);
+            if (withdrawal == null)
+            {
+                throw new Exception("Internal server error");
+            }
+            return Ok(new { withdrawal = withdrawal, balance = account.Balance });
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
+    }
+
+    [HttpGet("balance/{aNumber}")]
+    public IActionResult GETbalanceById([FromRoute] string aNumber)
+    {
+        try
+        {
+            var account = accounts.FirstOrDefault(a => a.Number == aNumber);
+            if (account == null)
+            {
+                return NotFound($"Account with id: {aNumber} not found");
+            }
+            return Ok(account.Balance);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
+    }
+
 }
+
+
