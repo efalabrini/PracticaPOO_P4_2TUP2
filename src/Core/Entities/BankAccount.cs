631b37e1759ffde298bc8bd1157c9ffaf9ddb7b8
Lautaro Gonzalez <lautiasc10@gmail.com>
2025-08-19 12:57:25 -0300

    PR update: I am including missing files.

--- src/Core/Entities/BankAccount.cs
+++ src/Core/Entities/BankAccount.cs
@@ -5,7 +5,18 @@ public class BankAccount
     private static int s_accountNumberSeed = 1234567890;
     public string Number { get; }
     public string Owner { get; set; }
-    public decimal Balance { get; }
+    public decimal Balance
+    {
+        get
+        {
+            decimal balance = 0;
+            foreach (var item in _allTransactions)
+            {
+                balance += item.Amount;
+            }
+            return balance;
+        }
+    }
 
     public BankAccount(string name, decimal initialBalance)
     {
@@ -13,14 +24,32 @@ public class BankAccount
         s_accountNumberSeed++;
 
         Owner = name;
-        Balance = initialBalance;
+        MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
     }
+
+    private List<Transaction> _allTransactions = new List<Transaction>();
 
     public void MakeDeposit(decimal amount, DateTime date, string note)
     {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "La cantidad para depositar debe ser positvo.");
+        }
+        var deposit = new Transaction(amount, date, note);
+        _allTransactions.Add(deposit);
     }
 
     public void MakeWithdrawal(decimal amount, DateTime date, string note)
     {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "La cantidad para retirar debe ser positvo.");
+        }
+        if (Balance - amount < 0)
+        {
+            throw new InvalidOperationException("No tenes suficentes fondos para realizar el retiro.");
+        }
+        var withdrawal = new Transaction(-amount, date, note);
+        _allTransactions.Add(withdrawal);
     }
 }
