8474292089c4107bd4a23bb27600a8c55aba7f88
Victoria Fabi <victoria.fabi87@gmail.com>
2025-08-19 11:53:20 -0300

    Agrega Controller BackAccount

--- src/Core/Entities/BankAccount.cs
+++ src/Core/Entities/BankAccount.cs
@@ -1,16 +1,30 @@
-namespace Classes;
-
-public class BankAccount
+namespace Core.Entities
 {
-	public string Number { get; }
-	public string Owner { get; set; }
-	public decimal Balance { get; }
+    public class BankAccount
+    {
+        private static int s_accountNumberSeed = 1234567890;
 
-	public void MakeDeposit(decimal amount, DateTime date, string note)
-	{
-	}
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get;  }
 
-	public void MakeWithdrawal(decimal amount, DateTime date, string note)
-	{
-	}
+        // Constructor
+        public BankAccount(string owner, decimal initialBalance)
+        {
+            Owner = owner;
+            Balance = initialBalance;
+            Number = s_accountNumberSeed.ToString();
+            s_accountNumberSeed++;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+           
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            
+        }
+    }
 }
