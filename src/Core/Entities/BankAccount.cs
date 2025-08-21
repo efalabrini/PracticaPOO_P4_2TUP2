f549ccc4e0a797832a16bfcd69535b9707012f99
Federico Luna <federicolun4@gmail.com>
2025-08-21 16:53:21 -0300

    Implement BankAccount and Transaction classes with deposit, withdrawal, and account history features; add BankAccountController and TransactionController for account management.

--- src/Core/Entities/BankAccount.cs
+++ src/Core/Entities/BankAccount.cs
@@ -5,23 +5,87 @@ public class BankAccount
   private static int s_accountNumberSeed = 1234567890;
   public string Number { get; }
   public string Owner { get; set; }
-  public decimal Balance { get; }
+  public decimal Balance
+  {
+    get
+    {
+      decimal balance = 0;
+      foreach (var item in _allTransactions)
+      {
+        balance += item.Amount;
+      }
+
+      return balance;
+    }
+  }
+
+  private static readonly List<BankAccount> accounts = [];
+
+  public static void AddAccount(BankAccount account)
+  {
+    accounts.Add(account);
+  }
+
+  public static List<BankAccount> GetAllAccounts()
+  {
+    return accounts;
+  }
+
+  public static BankAccount? GetAccountByNumber(string number)
+  {
+    BankAccount? account = accounts.FirstOrDefault(a => a.Number == number);
+
+    return account;
+  }
 
   public void MakeDeposit(decimal amount, DateTime date, string note)
   {
+    if (amount < 0)
+    {
+      throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+    }
+    var deposit = new Transaction(amount, date, note);
+    _allTransactions.Add(deposit);
   }
 
   public void MakeWithdrawal(decimal amount, DateTime date, string note)
   {
+    if (amount <= 0)
+    {
+      throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+    }
+    if (Balance - amount < 0)
+    {
+      throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+    }
+    var withdrawal = new Transaction(-amount, date, note);
+    _allTransactions.Add(withdrawal);
+  }
+
+  public string GetAccountHistory()
+  {
+    var report = new System.Text.StringBuilder();
+
+    decimal balance = 0;
+    report.AppendLine("Date\t\tAmount\tBalance\tNote");
+    foreach (var item in _allTransactions)
+    {
+      balance += item.Amount;
+      report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+    }
+
+    return report.ToString();
   }
 
   public BankAccount(string name, decimal initialBalance)
   {
-    Owner = name;
-    Balance = initialBalance;
-
     Number = s_accountNumberSeed.ToString();
     s_accountNumberSeed++;
+
+    Owner = name;
+    MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
   }
+
+  private List<Transaction> _allTransactions = [];
 
 }
--- src/Web/Controllers/BankAcountController.cs
+++ src/Web/Controllers/BankAcountController.cs
@@ -9,6 +9,13 @@ public class BankAccountController : ControllerBase
   public IActionResult CreateBankAccount([FromQuery] string name)
   {
     BankAccount account = new(name, 0);
+    BankAccount.AddAccount(account);
     return Ok(account);
+  }
+
+  [HttpGet]
+  public IActionResult GetAllAccounts()
+  {
+    return Ok(BankAccount.GetAllAccounts());
   }
 }
