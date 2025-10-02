0c8edcd1e85d4928f9cccc9dd7742353df7823f1
Justina-R <justina.rey2602@gmail.com>
2025-10-02 17:09:16 -0300

    Add GlobalExceptionHandling Middleware

--- src/Web/Middlewares/GlobalExceptionHandlingMiddleware.cs
+++ src/Web/Middlewares/GlobalExceptionHandlingMiddleware.cs
@@ -1,19 +1,37 @@
+using Core.Exceptions;
+
 namespace Web.Middleware;
 
 public class GlobalExceptionHandlingMiddleware : IMiddleware
+{
+    private readonly ILogger<GlobalExceptionHandlingMiddleware> _logger;
+    public GlobalExceptionHandlingMiddleware(
+        ILogger<GlobalExceptionHandlingMiddleware> logger)
     {
-        private readonly ILogger<GlobalExceptionHandlingMiddleware> _logger;
-        public GlobalExceptionHandlingMiddleware(
-            ILogger<GlobalExceptionHandlingMiddleware> logger)
-        {
-            _logger = logger;
-        }
+        _logger = logger;
+    }
 
 
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
-        Console.WriteLine("Hola, yendo para rutear");
-        await next(context);
-        Console.WriteLine("Hola, volviendo para responder");
+        try
+        {
+            await next(context);
+        }
+        catch (AppValidationException ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            await context.Response.WriteAsync(ex.Message);
+        }
+        catch (NullReferenceException ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+            await context.Response.WriteAsync("La instancia de objeto que está buscando no existe NULLREFERENCE.");
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await context.Response.WriteAsync($"Error interno del servidor: {ex.Message}");
+        }
     }
-    }
+}
